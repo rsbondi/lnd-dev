7cac87eb5ae659c706cfbdcf144808ebb922f7a9
Richard Bondi <r_bondi@hotmail.com>
2019-06-16 14:45:38 -0500

    refactor

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
-	"strconv"
 	"strings"
 	"text/template"
 )
@@ -23,6 +22,8 @@ var cli *tview.InputField
 var list *tview.DropDown
 var cliresult *tview.TextView
 var currentnode, nNodes, nChannels string
+var aliases map[string]*alias
+var nodes map[string]*node
 
 func main() {
 	cliresult = tview.NewTextView().SetDynamicColors(true)
@@ -33,15 +34,12 @@ func main() {
 	currentnode = ""
 
 	form = tview.NewForm().
-		AddInputField("Number of Nodes", "", 5, nil, func(t string) {
-			_, err := strconv.Atoi(t)
-			if err != nil {
-				t = nNodes
-				return
-			}
+		AddInputField("Number of Nodes", "", 5, tview.InputFieldInteger, func(t string) {
 			nNodes = t
 		}).
-		AddInputField("Incoming Connections per Node", "", 5, nil, nil).
+		AddInputField("Incoming Connections per Node", "", 5, tview.InputFieldInteger, func(t string) {
+			nChannels = t
+		}).
 		AddButton("Ok", setUI).
 		AddButton("Cancel", func() {
 			app.Stop()
@@ -56,7 +54,7 @@ func main() {
 	}
 }
 
-func setUI() {
+func randomNames() *apiresults {
 	url := fmt.Sprintf("https://randomuser.me/api/?results=%s&inc=name", nNodes)
 	resp, err := http.Get(url)
 	if err != nil {
@@ -67,29 +65,19 @@ func setUI() {
 	decoder := json.NewDecoder(reader)
 	names := &apiresults{}
 	decoder.Decode(&names)
+	return names
+}
 
+func swapForm() {
 	col := tview.NewFlex().SetDirection(tview.FlexColumn)
 	col.AddItem(list, 40, 1, false)
 	col.AddItem(cli, 0, 1, true)
 	flex.AddItem(col, 3, 1, true)
 	flex.AddItem(cliresult, 0, 5, false)
 	flex.RemoveItem(form)
+}
 
-	tmpl, _ := template.New("view").Parse(configtemplate)
-	aliases := make(map[string]*alias)
-
-	nodes := make(map[string]*node)
-
-	cliresult.SetBorder(false).SetTitle("CLI Result (Ctrl+y)")
-
-	cliresult.SetInputCapture(func(key *tcell.EventKey) *tcell.EventKey {
-		if key.Key() == tcell.KeyCtrlL {
-			cliresult.SetText("")
-			nodes[currentnode].Buff = ""
-		}
-		return key
-	})
-
+func defineNodes(r []apiname) map[string]*alias {
 	dir, err := os.Getwd()
 	if err != nil {
 		panic(err)
@@ -103,7 +91,10 @@ func setUI() {
 		}
 	}
 
-	for i, n := range names.Results {
+	tmpl, _ := template.New("view").Parse(configtemplate)
+	aliases = make(map[string]*alias)
+
+	for i, n := range r {
 		var b bytes.Buffer
 		name := n.Name.Last
 		view := &cfgview{}
@@ -120,7 +111,76 @@ func setUI() {
 		defer f.Close()
 		_, err = f.Write(b.Bytes())
 	}
+	return aliases
 
+}
+
+func cliInputCapture(key *tcell.EventKey) *tcell.EventKey {
+	if key.Key() == tcell.KeyEnter {
+		text := cli.GetText()
+		cmdfmt := fmt.Sprintf("[#ff0000]# %s[white]\n", text)
+		fmt.Fprintf(cliresult, cmdfmt)
+		if text == "" {
+			fmt.Fprintf(cliresult, "Please provide a command to execute\n")
+			return key
+		}
+		args, err := parseCommandLine(text)
+		if err != nil {
+			fmt.Fprintf(cliresult, "%s\n", err.Error())
+		}
+		clicmd := strings.Split(*aliases[currentnode].Path, " ")
+		cliarg := []string{clicmd[1]}
+		cliargs := append(cliarg, args...)
+		cmd := exec.Command(clicmd[0], cliargs...)
+		cmd.Stdin = strings.NewReader("some input")
+		var out bytes.Buffer
+		cmd.Stdout = &out
+		err = cmd.Run()
+		if err != nil {
+			fmt.Fprintf(cliresult, "%s\n", err.Error())
+		}
+
+		fmt.Fprintf(cliresult, "%s\n", tview.Escape(out.String()))
+		nodes[currentnode].Buff += cmdfmt
+		nodes[currentnode].Buff += out.String()
+		nodes[currentnode].Cmds = append(nodes[currentnode].Cmds, cli.GetText())
+		*nodes[currentnode].CmdIndex = len(nodes[currentnode].Cmds)
+
+		cli.SetText("")
+	} else if key.Key() == tcell.KeyUp {
+		index := nodes[currentnode].CmdIndex
+		if *index > 0 {
+			*index = *index - 1
+		}
+		if *index >= 0 && *index < len(nodes[currentnode].Cmds) {
+			cli.SetText(nodes[currentnode].Cmds[*index])
+		}
+	} else if key.Key() == tcell.KeyDown {
+		index := nodes[currentnode].CmdIndex
+		if *index == len(nodes[currentnode].Cmds)-1 {
+			cli.SetText("")
+			*index = *index + 1
+			return key
+		}
+		if *index < len(nodes[currentnode].Cmds)-1 {
+			*index = *index + 1
+		}
+		if *index >= 0 && *index < len(nodes[currentnode].Cmds) {
+			cli.SetText(nodes[currentnode].Cmds[*index])
+		}
+	} else if key.Key() == tcell.KeyCtrlV {
+		clip, err := clipboard.ReadAll()
+		if err != nil {
+			fmt.Fprintf(cliresult, "%s\n", err.Error())
+		} else {
+			full := strings.Replace(clip, "\n", "", -1)
+			cli.SetText(fmt.Sprintf("%s%s", cli.GetText(), full)) // TODO: this only paste to end, fix for insert
+		}
+	}
+	return key
+}
+
+func populateList() {
 	aliasKeys := sortAliasKeys(aliases)
 	for _, a := range aliasKeys {
 		s := -1
@@ -155,75 +215,35 @@ func setUI() {
 
 	list.SetBorder(true).SetTitle("Nodes (Ctrl+n)")
 	list.SetCurrentOption(0)
+
+}
+
+func setUI() {
+	names := randomNames()
+	swapForm()
+
+	nodes = make(map[string]*node)
+
+	cliresult.SetBorder(false).SetTitle("CLI Result (Ctrl+y)")
+
+	cliresult.SetInputCapture(func(key *tcell.EventKey) *tcell.EventKey {
+		if key.Key() == tcell.KeyCtrlL {
+			cliresult.SetText("")
+			nodes[currentnode].Buff = ""
+		}
+		return key
+	})
+
+	aliases = defineNodes(names.Results)
+
+	populateList()
+
 	cli.
 		SetPlaceholder("Enter cli command - use Ctrl+v to paste (no shift)").
 		SetFieldBackgroundColor(tcell.ColorBlack).
 		SetFieldWidth(0).SetBorder(true).SetTitle("CLI (Ctrl+i) for CLI (Ctrl+y) for results")
 
-	cli.SetInputCapture(func(key *tcell.EventKey) *tcell.EventKey {
-		if key.Key() == tcell.KeyEnter {
-			text := cli.GetText()
-			cmdfmt := fmt.Sprintf("[#ff0000]# %s[white]\n", text)
-			fmt.Fprintf(cliresult, cmdfmt)
-			if text == "" {
-				fmt.Fprintf(cliresult, "Please provide a command to execute\n")
-				return key
-			}
-			args, err := parseCommandLine(text)
-			if err != nil {
-				fmt.Fprintf(cliresult, "%s\n", err.Error())
-			}
-			clicmd := strings.Split(*aliases[currentnode].Path, " ")
-			cliarg := []string{clicmd[1]}
-			cliargs := append(cliarg, args...)
-			cmd := exec.Command(clicmd[0], cliargs...)
-			cmd.Stdin = strings.NewReader("some input")
-			var out bytes.Buffer
-			cmd.Stdout = &out
-			err = cmd.Run()
-			if err != nil {
-				fmt.Fprintf(cliresult, "%s\n", err.Error())
-			}
-
-			fmt.Fprintf(cliresult, "%s\n", tview.Escape(out.String()))
-			nodes[currentnode].Buff += cmdfmt
-			nodes[currentnode].Buff += out.String()
-			nodes[currentnode].Cmds = append(nodes[currentnode].Cmds, cli.GetText())
-			*nodes[currentnode].CmdIndex = len(nodes[currentnode].Cmds)
-
-			cli.SetText("")
-		} else if key.Key() == tcell.KeyUp {
-			index := nodes[currentnode].CmdIndex
-			if *index > 0 {
-				*index = *index - 1
-			}
-			if *index >= 0 && *index < len(nodes[currentnode].Cmds) {
-				cli.SetText(nodes[currentnode].Cmds[*index])
-			}
-		} else if key.Key() == tcell.KeyDown {
-			index := nodes[currentnode].CmdIndex
-			if *index == len(nodes[currentnode].Cmds)-1 {
-				cli.SetText("")
-				*index = *index + 1
-				return key
-			}
-			if *index < len(nodes[currentnode].Cmds)-1 {
-				*index = *index + 1
-			}
-			if *index >= 0 && *index < len(nodes[currentnode].Cmds) {
-				cli.SetText(nodes[currentnode].Cmds[*index])
-			}
-		} else if key.Key() == tcell.KeyCtrlV {
-			clip, err := clipboard.ReadAll()
-			if err != nil {
-				fmt.Fprintf(cliresult, "%s\n", err.Error())
-			} else {
-				full := strings.Replace(clip, "\n", "", -1)
-				cli.SetText(fmt.Sprintf("%s%s", cli.GetText(), full)) // TODO: this only paste to end, fix for insert
-			}
-		}
-		return key
-	})
+	cli.SetInputCapture(cliInputCapture)
 
 	app.SetInputCapture(func(key *tcell.EventKey) *tcell.EventKey {
 		if key.Key() == tcell.KeyCtrlN {
